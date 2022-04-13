1518ba0261b0df07989bae5df1450d1ae6ce638c
0xF5T9 <truyenhaunhan@gmail.com>
2022-04-13 15:55:25 +0700

    update main

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -12,65 +12,26 @@
 
 using namespace std;
 
-//for demonstration only. never save your password in the code!
+/*	Tạo object từ classes để sử dụng constructor (Create the object to use constructors)	*/
 animation aniMainObj;
 cmysql cmysqlMainObj;
 menu menuMainObj;
 
 
 int main() {
-
-	//	Thiết lập thông số chương trình (Initializing the program parameters)	//
-	SetConsoleOutputCP(65001);
-	cmysqlMainObj.Connect();
-	cmysqlMainObj.ConnectDB();
+	/*	Thiết lập thông số chương trình (Initializing the program parameters)	*/
 	int luaChon;
 	bool q = false;
-	SetConsoleTitle(L"MySQL Test");
+	SetConsoleOutputCP(65001);	//	Set codepage UTF-8 - hỗ trợ tiếng việt
+	cmysqlMainObj.Connect();	//	Kết nối máy chủ MySQL (Connect to the MySQL Server)
+	cmysqlMainObj.ConnectDB();	//	Kết nối Database (Connect to the Database)
+	SetConsoleTitle(L"MySQL Connector with Demo Functions");
 
+	/*	Khởi chạy chương trình (Start the program)	*/
 	while (q == false) {
 		luaChon = menuMainObj.getInput();
 		q = menuMainObj.processInput(luaChon);
 	}
-	exit(0);
-
-	sql::Driver* driver;
-	sql::Connection* con;
-	sql::Statement* stmt;
-	sql::PreparedStatement* pstmt;
-	sql::ResultSet* result;
-	const std::string server = "tcp://localhost:3306";
-	const std::string username = "root";
-	const std::string password = "1284";
-
-	exit(0);
-
-	/*	Read and load data from table into variables	*/
-	pstmt = con->prepareStatement("SELECT * FROM account;"); //	Prepare statement to execute
-	result = pstmt->executeQuery();
-	string userdata[1000][3]; 
-	int x = 0;
-	int y;
-	while (result->next()) {
-		//printf("Reading from table=(%d, %s, %s)\n", result->getInt(1), result->getString(2).c_str(), result->getString(3).c_str());
-		y = result->getInt(1);
-		string z = to_string(y);
-		userdata[x][0] = z;
-		userdata[x][1] = result->getString(2);
-		userdata[x][2] = result->getString(3);
-		x++;
-	}
-	for (int i = 0; i < 3; i++) cout << userdata[0][i] << " ";
-	cout << endl;
-	for (int i = 0; i < 3; i++) cout << userdata[1][i] << " ";
-	cout << endl;
-	for (int i = 0; i < 3; i++) cout << userdata[2][i] << " ";
-	cout << endl;
-	delete result;
-	delete pstmt;
-	/*-----------------------------------------------------------------------------------------------------------------------------*/
-	delete con;
-	system("pause");
+	/*	Kết thúc chương trình (Ending the program)	*/
 	return 0;
-	
 }
